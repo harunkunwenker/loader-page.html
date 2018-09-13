d8ca1e216cd9d655b5688a35d8aa751e31636c8f
Harun Kun Wenker <38748942+harunkunwenker@users.noreply.github.com>
2018-09-13 10:50:28 +0700

    Create ads.trafficjunky.js

---
+++ ads.trafficjunky.js
@@ -0,0 +1,22 @@
+<!--a img   { border: 0; } body    { margin: 0; padding: 0; text-align: center; background-color: #000000} --> </style><script type="text/javascript" src="//media.trafficjunky.net/flash/js/swfobject.js"></script><script type="text/javascript">
+	var ads = [];
+function loadAds() {
+for(i=0;i<ads.length;i++) {
+			ads[i]();
+		}
+	}
+	</script> 
+    
+ <script>
+	page_params.zoneDetails['oh82'] = {
+						'height' 		: '250',
+		'width' 		: '950',
+		'zoneId'		: '5',
+		'special'		: '',
+		'dynamicId' 	: 'lfjf654dww2zzuo',
+		'adTypeParent' 	: 'f',
+		'adTypeChild' 	: '',
+					'preroll_change_url': 'https://cdn1d-static-shared.phncdn.com/iframe-1.1.5.html#zone_id=5&amp;channel%5Bcontext_page_type%5D=category&amp;channel%5Bcontext_tag%5D=Nutaku+Aug+5-8&amp;site_id=2&amp;channel%5Bsite%5D=pornhub',
+				'frameId'       : 'oh82'
+	};
+</script> 
